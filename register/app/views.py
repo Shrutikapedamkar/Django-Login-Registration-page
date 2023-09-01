faa166e81225394ba64351e157c4bea4979ec900
Shrutika Pedamkar <47322770+Shrutikapedamkar@users.noreply.github.com>
2023-09-01 20:17:12 +0100

    Adding files

---
+++ register/app/views.py
@@ -0,0 +1,54 @@
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User, auth
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == "POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username = username).exists():
+                messages.info(request, 'This Email address is already registered')
+                return redirect(register)
+            else:
+                user = User.objects.create_user(username = username, password = password, email = email,
+                                                first_name = first_name, last_name = last_name)
+                user.set_password(password)
+                user.is_staff = True
+                user.save()
+                print("Success!")
+                return redirect('login_user')
+
+    else:
+        print("This is not post method")
+        return render(request, "register.html")
+    
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = username, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, "Invalid Username or Password")
+            return redirect('login_user')
+
+    else:
+        return render(request, 'login.html')
+    
+def logout_user(request):
+    auth.logout(request)
+    return redirect('home')
